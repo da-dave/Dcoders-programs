3f65cdbeeb1cc21e5196396bd9e870a524a40cdd
Akhi <achappid@charlotte.edu>
2025-06-24 21:27:56 -0400

    feat: initial commit with good and bad versions of calculator

---
+++ good_calculator.py
@@ -0,0 +1,7 @@
+"""Simple calculator using clean& documented code
+Principles used from lecture: KISS, DRY, Document Your Code, and Clean Code
+"""
+
+def add(a, b):
+    # Return the sum of a and b
+    return a + b
